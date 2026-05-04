c1403f205693e13e8fce7697600206da47cb32c9
Mustapha <mousjolicoeur@gmail.com>
2026-05-04 18:50:08 +0200

    fix: UTF-8 charset + suppression BOM

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
+        <meta charSet="utf-8" />
         <AgenticSEO />
       </head>
       <body className="min-h-full flex flex-col">
